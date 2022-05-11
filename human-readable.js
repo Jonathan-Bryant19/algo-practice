69d7406e1576aba95e904f3db26d277f218fd372
Jonathan Bryant <spectrum19tech@gmail.com>
2022-05-11 05:43:07 -0700

    Finished time conversion algorithm

--- human-readable.js
+++ human-readable.js
@@ -8,24 +8,31 @@ PC:
 1. Test for hours and calculate
 2. Test for minutes and calculate
 3. Test for seconds and calculate
+45296
 */
-
-
 
 function humanReadable(seconds) {
     let hours = "00"
     let minutes = "00"
-    let ticks = `${seconds}`
+    let ticks
     if (seconds/3600 >= 1) {
-        // If less than 10 return "0" + hours/minutes/ticks
-        hours = Math.floor(seconds/3600)
-        minutes = Math.floor(((seconds/3600) - hours) * 60)
-        ticks = Math.ceil(((((seconds/3600) - hours) * 60) - minutes) * 60)
+        hours = Math.floor(seconds/3600) < 10 ? "0" + Math.floor(seconds/3600) : Math.floor(seconds/3600)
+        minutes = Math.floor(((seconds/3600) - hours) * 60) < 10 ? "0" + Math.floor(((seconds/3600) - hours) * 60) : Math.floor(((seconds/3600) - hours) * 60)
+        ticks = Math.round(((((seconds/3600) - hours) * 60) - minutes) * 60) < 10 ? "0" + Math.round(((((seconds/3600) - hours) * 60) - minutes) * 60) : Math.round(((((seconds/3600) - hours) * 60) - minutes) * 60)
     } else if (seconds/60 >= 1) {
-        minutes = Math.floor(seconds/60)
-        ticks = Math.floor(((seconds/60) - minutes) * 60)
-    } 
+        minutes = Math.floor(seconds/60) < 10 ? "0" + Math.floor(seconds/60) : Math.floor(seconds/60)
+        ticks = Math.floor(((seconds/60) - minutes) * 60) < 10 ? "0" + Math.floor(((seconds/60) - minutes) * 60) : Math.floor(((seconds/60) - minutes) * 60)
+    } else {
+        if (seconds === 60) {
+          minutes = "01"
+          ticks = "00"
+        }  else if (seconds < 10) {
+          ticks = "0" + seconds
+        } else {
+          ticks = seconds
+        }
+    }
     return `${hours}:${minutes}:${ticks}`;
 }
 
-console.log(humanReadable(3600))
+console.log(humanReadable(0))
