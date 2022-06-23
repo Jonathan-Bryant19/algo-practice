78aa80b3d3f85138b906ab5b84a0fdc8eb7a832f
Jonathan Bryant <spectrum19tech@gmail.com>
2022-06-22 22:59:49 -0700

    Daily Practice 6.22.22
    Complete daily practice.

---
+++ with-notes/2022-06/multiply.js
@@ -0,0 +1,20 @@
+/*
+* Date: 6.22.
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+* In My Own Words: 
+    Create a function that returns the product of two numbers.
+* Test Conditions: 
+    multiply(2, 3) // 6
+    multiply(-2, 3) // -6
+* Pseudocode: 
+    Return the product. This is a gimme. 
+* Notes After Attempt:
+    I could have made an effort to at least check for proper inputs like data type. 
+*/
+
+function multiply(a, b) {
+    return a * b
+}
+
+console.log(multiply(2, -3))
