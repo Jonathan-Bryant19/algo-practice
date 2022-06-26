c77a1307339947b8448c5362384e47a01e5298a0
Jonathan Bryant <spectrum19tech@gmail.com>
2022-06-25 20:35:33 -0700

    Daily Practice 6.25.22
    Complete Daily Practice

---
+++ with-notes/2022-06/make-upper-case.js
@@ -0,0 +1,19 @@
+/*
+* Date: 6.25.22
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+* In My Own Words: 
+    Convert a string to uppercase.
+* Test Conditions: 
+    "hello" ==> "HELLO"
+* Pseudocode: 
+    to upper case
+* Notes After Attempt:
+
+*/
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+
+console.log(makeUpperCase("hello"))
