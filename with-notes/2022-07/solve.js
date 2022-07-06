f6897253a16dcdac093f5a823597c5853c504eff
Jonathan Bryant <spectrum19tech@gmail.com>
2022-07-05 20:51:07 -0700

    Daily Practice 7.5.22
    Complete daily practice.

--- with-notes/2022-07/solve.js
+++ with-notes/2022-07/solve.js
@@ -11,5 +11,16 @@
 * Pseudocode: 
     1. Set variable to count lowercase
 * Notes After Attempt:
-
+    Best practices either counted both upper and lower (not necessary) or used regex (I really need to learn that.)
 */
+
+function solve(s){
+    let lowerCount = 0
+    const half = Math.floor(s.length / 2)
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i].toLowerCase()) lowerCount++
+    }
+    return lowerCount >= half ? s.toLowerCase() : s.toUpperCase()
+}
+
+console.log(solve("COde"))
