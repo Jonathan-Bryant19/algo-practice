839744d0850810e5c0f536e82d4bc16f1b082bbd
Jonathan Bryant <spectrum19tech@gmail.com>
2022-07-11 08:58:25 -0700

    Daily Practice 7.11.11
    Complete daily practice.

---
+++ with-notes/2022-07/get-volume-of-cuboid.js
@@ -0,0 +1,20 @@
+/*
+* Date: 7.11.22
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+* In My Own Words: 
+    Return the product of the three variables.
+* Test Conditions: 
+    getVolumeOfCuboid(1,2,2), 4
+    getVolumeOfCuboid(6.3,2,5), 63
+* Pseudocode: 
+    Just return product.
+* Notes After Attempt:
+
+*/
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+}
