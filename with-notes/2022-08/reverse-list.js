0b9ab10dd5ddabe4dea3753f1a01ef50f7802002
Jonathan Bryant <spectrum19tech@gmail.com>
2022-08-09 20:12:11 -0700

    Daily Practice 8.9.22
    Complete daily practice.

---
+++ with-notes/2022-08/reverse-list.js
@@ -0,0 +1,20 @@
+/*
+* Date: 8.9.22
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/javascript
+* In My Own Words: 
+    Reverse an array.
+* Test Conditions: 
+    reverseList([1,2,3,4]), [4,3,2,1]
+    reverseList([3,1,5,4]), [4,5,1,3]
+* Pseudocode: 
+    Reverse it.
+* Notes After Attempt:
+
+*/
+
+function reverseList(list) {
+    return list.reverse()
+}
+
+console.log(reverseList([1,2,3,4]))
