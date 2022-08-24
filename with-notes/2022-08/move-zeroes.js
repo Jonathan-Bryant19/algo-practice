19dcb9dc488322a64dc25e0952fe73d8eb9d4d2d
Jonathan Bryant <spectrum19tech@gmail.com>
2022-08-24 11:52:15 -0700

    Daily Practice 8.24.22
    Complete daily practice.

--- with-notes/2022-08/move-zeroes.js
+++ with-notes/2022-08/move-zeroes.js
@@ -12,9 +12,17 @@
 * Pseudocode: 
     Map, splice, and push.
 * Notes After Attempt:
-
+    Whenever I splice there's a re-indexing of the rest of the array which gives it a worst case of O(n^2). 
 */
 
 var moveZeroes = function(nums) {
-    
+    for (let i = nums.length -1; i >= 0; i--) {
+        if (nums[i] === 0) {
+            nums.splice(i, 1)
+            nums.push(0)
+        }
+    }
+    return nums
 };
+
+console.log(moveZeroes([0,1,0,3,12]))
