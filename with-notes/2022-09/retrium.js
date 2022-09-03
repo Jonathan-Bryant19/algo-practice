ed0e2ec61e653f07ca8587382305f1a31adc6586
Jonathan Bryant <spectrum19tech@gmail.com>
2022-09-02 20:00:59 -0700

    Complete loop structure for identifying which row and column to check

--- with-notes/2022-09/retrium.js
+++ with-notes/2022-09/retrium.js
@@ -12,15 +12,16 @@
     BLINKER_BOARD and TOAD_BOARD alternate between two configurations (check gifs)
     GLIDER_BOARD continues moving.
 * Pseudocode: 
-    1. Find the upperLeft limit (lowest x, highest y)
-    2. Find the lowerRight limit (highest x, lowest y)
-    3. Outer loop over the object keys, inner loop over the array values
+    1. Outer loop over the object keys, inner loop over the array values
         *** These loops assume the keys will be ordered and the array values will be ordered. ***
         a. Outer
             1. From lowest key -1 to highest key + 1
         b. Inner
-            1. From array[0] - 1 to array[array.length - 1] + 1 
-        Count neighbors for every 
+            1. From array[0] - 1 to array[array.length - 1] + 1 (refactor)
+        Count number of live cells per 9 block chunk.
+        * If n = 3, center cell is alive
+        * If n = 4, center cell retains current state
+        * Else center cell is dead 
 * Notes After Attempt:
 
 */
@@ -31,7 +32,6 @@ const BLOCK_BOARD = {
     2: [1,2],
     3: [],
 };
-
 const BEEHIVE_BOARD = {
     0: [],
     1: [2,3],
@@ -39,7 +39,6 @@ const BEEHIVE_BOARD = {
     3: [2,3],
     4: []
 }
-
 const BLINKER_BOARD = {
     0: [],
     1: [2],
@@ -47,7 +46,6 @@ const BLINKER_BOARD = {
     3: [2],
     4: []
 }
-
 const TOAD_BOARD = {
     0: [],
     1: [],
@@ -56,7 +54,6 @@ const TOAD_BOARD = {
     4: [],
     5: []
 }
-
 const GLIDER_BOARD = {
     0: [],
     1: [3],
@@ -67,9 +64,28 @@ const GLIDER_BOARD = {
 }
 
 function getNextGeneration(inputBoard) {
-    const outputBoard = inputBoard
+    const outputBoard = {...inputBoard}
+    
 
+    // Count Neighbors Helper Function
+    function countNeighbors(row, col) {
+        return `${row}:${col}`
+    }
+    
+    
+    // Grab keys to determine where to start and stop
+    const keysArr = Object.keys(inputBoard)
+    // Loop over each row
+    for (let i = keysArr[0] - 1; i <= parseInt(keysArr[keysArr.length-1]) + 1; i++) {
+        if (inputBoard[i] && inputBoard[i][0] !== undefined) {
+            for (let j = (inputBoard[i][0]) - 1; j <= inputBoard[i][inputBoard[i].length - 1] + 1; j++) {
+                
+            }
+        } else {
+            console.log("Nothing in row ", i, "!!!")
+        }
+    }
     return outputBoard
 }
 
-console.log(getNextGeneration(BLINKER_BOARD))
+console.log(getNextGeneration(GLIDER_BOARD))
