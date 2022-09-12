fd7c53e914e5e6823a2e4a5673238cf89fb9ff1f
Jonathan Bryant <spectrum19tech@gmail.com>
2022-09-11 20:33:48 -0700

    Daily Practice 9.11.22
    Complete daily practice.

---
+++ with-notes/2022-09/combine-names.js
@@ -0,0 +1,19 @@
+/*
+* Date: 9.11.22
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/55f73f66d160f1f1db000059/train/javascript
+* In My Own Words: 
+    Concat and return two strings.
+* Test Conditions: 
+    combineNames('James', 'Stevens') // 'James Stevens'
+* Pseudocode: 
+
+* Notes After Attempt:
+
+*/
+
+function combineNames(first, last) {
+    return first + " " + last
+}
+
+console.log(combineNames('James', 'Stevens'))
