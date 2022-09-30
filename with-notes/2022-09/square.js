261586d9d9b8256e5c24336fb2fe24ae66adc404
Jonathan Bryant <spectrum19tech@gmail.com>
2022-09-30 10:32:23 -0700

    Daily Practice 9.30.22
    Complete daily practice.

---
+++ with-notes/2022-09/square.js
@@ -0,0 +1,17 @@
+/*
+* Date: 9.30.22
+* Rank: 8 Kyu
+* Link: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+* In My Own Words: 
+    Return the square of a number.
+* Test Conditions: 
+    square(3), 9
+* Pseudocode: 
+
+* Notes After Attempt:
+
+*/
+
+function square(num) {
+    return Math.pow(num, 2)
+}
