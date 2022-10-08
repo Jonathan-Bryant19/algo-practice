b4c2c1b4d5a664728438e5928fe3fff233fb4407
Jonathan Bryant <spectrum19tech@gmail.com>
2022-10-08 13:13:26 -0700

    Quick commit so I can go work while my kid falls asleep for a nap

---
+++ with-notes/2022-10/nb-months.js
@@ -0,0 +1,18 @@
+/*
+* Date: 10.8.22
+* Rank: 6 Kyu
+* Link: https://www.codewars.com/kata/554a44516729e4d80b000012/train/javascript
+* In My Own Words: 
+    Given an old car price, new car price, savings per month, and percent loss every month, return how many months to save for new car based on current values of both cars. Also return the amount of money left over. Every two months, the percent loss per month increases by 0.5.
+* Test Conditions: 
+    nbMonths(2000, 8000, 1000, 1.5), [6, 766])
+    nbMonths(12000, 8000, 1000, 1.5) ,[0, 4000])
+* Pseudocode: 
+
+* Notes After Attempt:
+
+*/
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+    
+}
