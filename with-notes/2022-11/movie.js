adeed68dd6bd5e109dcb42da04a1442e61bfcbcd
Jonathan Bryant <spectrum19tech@gmail.com>
2022-12-09 23:56:23 -0800

    Daily Practice 12.9.22
    Complete daily practice.

---
+++ with-notes/2022-11/movie.js
@@ -0,0 +1,20 @@
+/*
+* Date: 12.9.22
+* Rank: 7 Kyu
+* Link: https://www.codewars.com/kata/562f91ff6a8b77dfe900006e/train/javascript
+* In My Own Words: 
+  Determine how many times ones must go to the movies before a bulk system is less expensive.
+* Test Conditions: 
+  movie(500, 15, 0.9), 43)
+  movie(100, 10, 0.95), 24)
+* Pseudocode: 
+
+* Notes After Attempt:
+
+*/
+
+function movie(card, ticket, perc) {
+  let count = 0
+  let card = 0
+  let tickets = 0
+}
